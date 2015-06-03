523497408ffccb15fa87faf9d069067cf188c853
cpyarger <cpyarger@gmail.com>
2015-06-03 00:23:38 -0400

    test

--- script.linux.xbmcwicd/resources/lib/new.py
+++ script.linux.xbmcwicd/resources/lib/new.py
@@ -1,3 +1,21 @@
+#!/usr/bin/python
+
+""" Scriptable command-line interface. """
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
 import optparse
 import dbus
 import dbus.service
@@ -5,7 +23,7 @@ import sys
 from wicd import misc
 from wicd.translations import _
 
-misc.RenameProcess('xbmc-wicd')
+misc.RenameProcess('wicd-cli')
 
 exit_status = 0
 
@@ -16,7 +34,6 @@ else:
     DBusGMainLoop(set_as_default=True)
 
 bus = dbus.SystemBus()
-
 try:
     daemon = dbus.Interface(
         bus.get_object('org.wicd.daemon', '/org/wicd/daemon'),
@@ -37,4 +54,9 @@ try:
 except dbus.DBusException:
     print 'Error: Could not connect to the daemon. ' + \
         'Please make sure it is running.'
+    sys.exit(3)
+
+if not daemon:
+    print 'Error connecting to wicd via D-Bus. ' + \
+        'Please make sure the wicd service is running.'
     sys.exit(3)
